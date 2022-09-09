b01688c214580b5206795a08214f25dd087f348d
spektres <spektres@mail.ru>
2022-09-09 13:31:00 +0300

    pass-gen start

---
+++ password-generator.py
@@ -0,0 +1,8 @@
+import random
+
+DIGITS = '0123456789'
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PUNCTUATION = '!#$%&*+-=?@^_'
+
+chars = ''
