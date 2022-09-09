78e0c5ea0d4ca64bc3e18cf964a529a78cdd01f2
spektres <spektres@mail.ru>
2022-09-09 12:13:59 +0300

    guess-numeric.py done

--- guess-numeric.py
+++ guess-numeric.py
@@ -1,26 +1,32 @@
-# import random
+import random                    # подключаем модуль
 
-# number = random.randint(1, 100)
-# print("Добро пожаловать в числовую угадайку")
+number = random.randint(1, 101)  # генерируем случайное число от 1 до 100
 
-# print("Введите число от 1 до 100")
+print('Добро пожаловать в числовую угадайку')
 
-# def is_valid(str number):
-#     if 0 < number < 100: return True
-#     else: return False, print("А может быть все-таки введем целое число от 1 до 100?")
+def is_valid(num):
+    if num.isdigit():
+        num = int(num)
+        if 1 <= num <= 100:
+            return True
+        else:
+            return False
+    else:
+        return False
 
+while True:
+    response = input('Введите число от 1 до 100:')
+    if not is_valid(response):
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        continue
+    response = int(response)
 
+    if response < number:
+        print('Ваше число меньше загаданного, попробуйте еще разок')
+    elif response > number:
+        print('Ваше число больше загаданного, попробуйте еще разок')
+    else:
+        print('Вы угадали, поздравляем!')
+        break
 
-
-
-
-
-
-# def compute_hypotenuse(a, b):
-#     c = (a ** 2 + b ** 2) ** 0.5
-#     return c
-
-# x = int(input())
-# y = int(input())
-
-# print(compute_hypotenuse(x, y))
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
