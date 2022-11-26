b4bec510c46aa35fbaa5507af60491e034f9f9f8
spektres <spektres@mail.ru>
2022-11-26 13:06:52 +0300

    sum

---
+++ lections/python-lection-4/calc/main.py
@@ -0,0 +1,5 @@
+# Точка входа (заставляет работать приложение)
+
+import controller as c
+
+c.button_click()
---
+++ lections/python-lection-4/calc/view.py
@@ -0,0 +1,6 @@
+def view_data(data):
+    print(f'Сумма чисел равна: {data}')
+
+
+def get_value():
+    return int(input('value = '))
